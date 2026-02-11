39e272693f17e954d52cb8a99132e9567ab63b37
Martin Orsag <martin.orsag108@gmail.com>
2026-02-11 10:35:50 +0100

    Switch, spread operator

--- src/app/structural-switch/structural-switch.ts
+++ src/app/structural-switch/structural-switch.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { TODOS } from '../model/mock-data';
 
 @Component({
   selector: 'app-structural-switch',
@@ -8,6 +9,8 @@ import { Component } from '@angular/core';
 })
 export class StructuralSwitch {
   grade: number = 0;
+  status = signal('draft');
+  listTodos = signal(TODOS);
 
   setGrade(x: number) {
     this.grade = x;
